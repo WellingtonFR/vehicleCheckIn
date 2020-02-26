5f0f758d0107196acca4ecee0bb4560a4c1d9425
WellingtonFR <wellingtondfr@gmail.com>
2020-02-26 11:14:56 -0300

    string de conexão

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production") {
-    module.exports = { mongoURI: "mongodb+srv://WellingtonFR:8eLYWr8rTkKw3DWj@cluster0-mkdnh.mongodb.net/test?retryWrites=true&w=majority" }
+    module.exports = { mongoURI: "mongodb+srv://WellingtonFR:8eLYWr8rTkKw3DWj@cluster0-mkdnh.mongodb.net/checkinvehicle?retryWrites=true&w=majority" }
 } else {
     module.exports = { mongoURI: "mongodb://localhost:32768/checkinvehicle" }
 }
